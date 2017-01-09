91a83a7f2d9827079779188278934db6d8e052ea
SagarGaniga <2015sagar.ganiga@ves.ac.in>
2017-01-09 14:44:53 +0530

    Add files via upload

--- CChecker.java
+++ CChecker.java
@@ -105,7 +105,7 @@ public class CChecker
                     {
                         flager = flag = true;
                     }
-                    
+                    //hiabid
                     //for BitWise LS (<<)
                     if((s.charAt(i)=='<') && ((s.charAt(i+1)=='<') || (s.charAt(i-1)=='<') ) && (i+1)<s.length() && (i-1)>=0)
                     {
